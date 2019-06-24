eed7825a7987df55513d2cf56cfe1d6a541928a5
Kim Il-Hwan <kih3159@naver.com>
2019-06-24 20:11:35 +0900

    version 1.0.0 2019.06.24

---
+++ src/Brute_Force/Q_14225.java
@@ -0,0 +1,38 @@
+package Brute_Force;
+
+import java.util.Scanner;
+
+public class Q_14225 {
+    static boolean[] part_sum = new boolean[2000010]; // i가 부분순열의 합으로 가능한지 체크
+    static int n;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt(); // 수열의 크기
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        //Arrays.sort(arr);
+
+        solve(0, 0, arr);
+
+        for (int i = 1; ; i++) {
+            if (!part_sum[i]) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+
+    private static void solve(int i, int sum, int[] arr) {
+        if (i == n) {
+            part_sum[sum] = true;
+            return;
+        }
+        solve(i + 1, sum + arr[i], arr);
+        solve(i + 1, sum, arr);
+    }
+}
---
+++ src/Brute_Force/Q_2916.java
@@ -0,0 +1,39 @@
+package Brute_Force;
+
+import java.util.Scanner;
+
+public class Q_2916 {
+    static int angle[]; // 갖고 있는 각도
+    static boolean check_angle[] = new boolean[360]; // 각도를 계산 할 수 있는지, 없는지
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // 만들 수 있는 각의 수
+        int k = sc.nextInt(); // 현우가 퀴즈로 외칠 각의 갯수
+
+        angle = new int[n];
+        for (int i = 0; i < n; i++) {
+            angle[i] = sc.nextInt();
+        }
+        check_angle[0] = true;
+        for (int i = 0; i < n; i++) {
+            // 반복 횟수
+            for (int cnt = 0; cnt < 360; cnt++) {
+                // 만들수 있는 각도
+                for (int cur_angle = 0; cur_angle < 360; cur_angle++) {
+                    if (!check_angle[cur_angle]) continue;
+                    check_angle[(cur_angle + angle[i]) % 360] = true;
+                    check_angle[(cur_angle - angle[i] + 360) % 360] = true;
+                }
+            }
+        }
+        while (k-- > 0) {
+            int quiz_angle = sc.nextInt();
+            if (check_angle[quiz_angle]) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
