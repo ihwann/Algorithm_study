f3cb199e3ae7449bb9ebc5b6f545782f33559ace
ilhwan <ilhwan@naver.com>
2022-03-09 18:19:34 +0900

    # 프로그래머스 문제추가

---
+++ src/Graph_and_BFS_DFS/TooHot.java
@@ -0,0 +1,17 @@
+package Graph_and_BFS_DFS;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TooHot {
+    public static void main(String[] args) {
+
+
+        String s = "ABCD";
+
+        System.out.println("s.replace(\"A\") = " + s.replace(String.valueOf(s.charAt(1)), " "));
+    }
+}
---
+++ src/Programmers/SkillTree.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+public class SkillTree {
+    public static void main(String[] args) {
+
+        String skill = "CBDK";
+        String[] skill_trees = {"BACDE", "CBADF", "AECB", "BDA"};
+
+        int answer = 0;
+        for (String skill_tree : skill_trees) {
+            for (int i = 0; i < skill_tree.length(); i++) {
+                String s = String.valueOf(skill_tree.charAt(i));
+                if (!skill.contains(s)) {
+                    skill_tree = skill_tree.replace(s, " ");
+                }
+            }
+            String s = skill_tree.replaceAll(" ", "");
+            if(s.equals(skill.substring(0, s.length()))) answer+=1;
+        }
+
+        System.out.println("answer = " + answer);
+    }
+}
